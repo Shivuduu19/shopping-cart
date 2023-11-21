e0d0a6b8c25195e115a86402aa6fadf3eb2a426b
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2023-11-21 14:20:40 +0530

    initial structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+const Header = () => {
+  return (
+    <>
+      <HeaderWrapper>
+        <h1>shiva</h1>
+      </HeaderWrapper>
+    </>
+  );
+};
+const HeaderWrapper = styled.header`
+  background-color: red;
+`;
+
+export default Header;
